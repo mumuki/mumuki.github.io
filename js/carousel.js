1b5a3667b585528d7a41ae97581c767196bb26f4
Agustin Pina <aguspina87@gmail.com>
2016-08-29 11:15:22 -0300

    Improve carousel

---
+++ js/carousel.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $(".carousel").carousel({
+    interval: 2000
+  });
+  $(".carousel").on("slid", function() {
+    var to_slide;
+    to_slide = $(".carousel-item.active").attr("data-slide-no");
+    $(".myCarousel-target.active").removeClass("active");
+    $(".carousel-indicators [data-slide-to=" + to_slide + "]").addClass("active");
+  });
+  $(".myCarousel-target").on("click", function() {
+    $(this).preventDefault();
+    $(".carousel").carousel(parseInt($(this).attr("data-slide-to")));
+    $(".myCarousel-target.active").removeClass("active");
+    $(this).addClass("active");
+  });
+});
